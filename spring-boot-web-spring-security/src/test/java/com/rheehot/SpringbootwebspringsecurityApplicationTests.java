3eac23e3c3f1cdbb3459985e041ef4a7df7718c4
LAPTOP-29RV9SMV\rheeh <rheehot@gmail.com>
2018-08-02 14:44:12 +0900

    spring-boot-web-spring-security coding

---
+++ spring-boot-web-spring-security/src/test/java/com/rheehot/SpringbootwebspringsecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rheehot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootwebspringsecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
