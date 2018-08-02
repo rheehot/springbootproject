2a704a3bdd0cac0b85f8042c0c30fa09d0e971c9
LAPTOP-29RV9SMV\rheeh <rheehot@gmail.com>
2018-08-02 17:17:56 +0900

    spring boot slf4j logging example

---
+++ sbsecurityjdbcthymeleaf/readme.md
@@ -0,0 +1,10 @@
+Spring Boot + Spring Security + JDBC + Thymeleaf
+
+This document is based on:
+Spring Boot 2.x
+Spring Security
+Spring JDBC
+Thymeleaf
+Database: MySQL
+
+reference : https://o7planning.org/en/11543/create-a-login-application-with-spring-boot-spring-security-spring-jdbc
---
+++ sbsecurityjdbcthymeleaf/src/main/java/com/rheehot/sbsecurity/SbsecurityjdbcthymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.rheehot.sbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsecurityjdbcthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbsecurityjdbcthymeleafApplication.class, args);
+    }
+}
